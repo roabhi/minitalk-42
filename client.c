34d4f1521c51691050e8d73d86ca0072fce9b75d
Roberto Abril hidalgo <rabril-h@car2s1.42barcelona.com>
2022-04-04 21:47:13 +0200

    working minitalk. Need to polish output from server so it prints each word on a new line

--- client.c
+++ client.c
@@ -6,31 +6,37 @@
 /*   By: rabril-h <rabril-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 17:02:43 by rabril-h          #+#    #+#             */
-/*   Updated: 2022/04/01 21:17:16 by rabril-h         ###   ########.fr       */
+/*   Updated: 2022/04/04 21:45:57 by rabril-h         ###   ########.bcn      */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
 #include "minitalk.h"
 
-void	decoder(unsigned char ch)
+void	decoder(unsigned char ch, pid_t pid)
 {
 	int				c;
 	unsigned int	base;
 
-	ft_printf("El caracter vale %d \n", ch);
 	c = 7;
 	base = 128;
 	while (c >= 0)
 	{
 		if (ch < base)
-			ft_printf("imprime 0 \n");
+		{
+			ft_putchar_cnt('0');
+			kill(pid, SIGUSR1);
+		}			
 		else
 		{
-			ft_printf("imprime 1 \n");	
-			ch = ch - base;
+			ft_putchar_cnt('1');
+			kill(pid, SIGUSR2);	
+			ch = ch - base;			
 		}
 		base = base / 2;
 		c--;
+		usleep(200);
 	}
 }
 
@@ -39,16 +45,17 @@ int	main(int args, char **argv)
 	int		_args;
 	char	*params;
 	int		counter;
+	int		server_pid;
 
 	_args = args;
 	params = *argv;
 	counter = 0;
 	if (args == 3)
 	{
-		//ft_printf("El numero de argumentos es %d, el PID del servidor es %s y el string a transmitir es %s\n\n", _args, argv[1], argv[2]);		
+		server_pid = ft_atoi(argv[1]);
 		while (argv[2][counter])
 		{
-			decoder(argv[2][counter]);
+			decoder(argv[2][counter], server_pid);
 			ft_putchar_cnt('\n');
 			counter++;
 		}
--- server.c
+++ server.c
@@ -3,17 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabril-h <rabril-h@student.42barc...>      +#+  +:+       +#+        */
+/*   By: rabril-h <rabril-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 18:09:05 by rabril-h          #+#    #+#             */
-/*   Updated: 2022/03/31 20:51:00 by rabril-h         ###   ########.bcn      */
+/*   Updated: 2022/04/04 21:39:47 by rabril-h         ###   ########.bcn      */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/signal.h>
+#include <unistd.h>
+#include <signal.h>
 #include "minitalk.h"
+
+void	decoder(int sig, siginfo_t *info,  void *context)
+{
+	(void)info;
+	(void)context;
+
+	static int	counter = 0;
+	static char	ch = 0;
+
+	if (sig == SIGUSR2)
+		ch = ch | 128 >> counter;
+	counter++;
+	//ft_printf("%d", counter);
+	if (counter == 8)
+	{
+		if (ch == '\0')
+			ft_putchar_cnt('\n');
+		else
+		{
+			write(1, &ch, 1);
+			ch = 0;
+			counter = 0;
+		}
+	
+	}
+
+	//if (sig == SIGUSR1)
+	//	ft_putchar_cnt('0');
+	//else
+	//	ft_putchar_cnt('1');
+	//kill(info->si_pid, SIGUSR2);
+	//if (!context)
+	//return;
+}
 
 int	main(void)
 {
-	ft_printf("El pid del server es %d", getpid());
+	//ft_printf("El pid del server es %d", getpid());
+	
+	struct sigaction 	sa1;
+	struct sigaction 	sa2;
+	pid_t				my_pid;
+
+	my_pid = getpid();
+	ft_printf("Server PID is %d", (int)my_pid);
+	ft_putchar_cnt('\n');
+	sa1.sa_flags = SA_SIGINFO;
+	sa1.sa_sigaction = decoder;
+	sigaction(SIGUSR1, &sa1, NULL);
+	sa2.sa_flags = SA_SIGINFO;
+	sa2.sa_sigaction = decoder;
+	sigaction(SIGUSR2, &sa2, NULL);
+	//signal(SIGUSR1, decoder);
+	//signal(SIGUSR2, decoder);
+	while (1)
+		sleep(5);
 	return (0);
 }
